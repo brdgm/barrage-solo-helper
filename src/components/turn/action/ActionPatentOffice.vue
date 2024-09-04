50217311c1164473f90ee11dddb9ae2395775d55
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-09-04 12:08:01 +0200

    patent office

--- src/components/turn/action/ActionPatentOffice.vue
+++ src/components/turn/action/ActionPatentOffice.vue
@@ -2,13 +2,16 @@
   <ActionBox :actionItem="actionItem" :navigationState="navigationState">
     <template #action>
       <div class="action">
-        <AppIcon type="action" name="patent-office" class="icon"/>
+        <HydroCost v-if="actionItem.hydroCost" :hydroCost="actionItem.hydroCost" class="hydroCost"/>
+        <AppIcon v-if="actionItem.constructionType" type="construction-type" :name="actionItem.constructionType" class="icon" :class="{[actionItem.constructionType]:true}"/>
+        <AppIcon type="action" name="patent-office" class="tile"/>
       </div>
     </template>
     <template #rules>
       <p class="fw-bold fst-italic" v-html="t('rules.actionItem.patent-office.intro')"></p>
-      <p v-html="t('rules.actionItem.patent-office.xxx')"></p>
-      <p v-html="t('rules.actionItem.patent-office.xxx')"></p>
+      <p v-html="t('rules.actionItem.patent-office.engineerPlacement')"></p>
+      <p v-html="t('rules.actionItem.patent-office.multipleTiles')"></p>
+      <p v-html="t('rules.actionItem.patent-office.noneAvailable')"></p>
     </template>
   </ActionBox>
 </template>
@@ -20,6 +23,7 @@ import AppIcon from '../../structure/AppIcon.vue'
 import Card, { ActionItem } from '@/services/Card'
 import BotNavigationState from '@/util/BotNavigationState'
 import ActionBox from '../ActionBox.vue'
+import HydroCost from '@/components/structure/HydroCost.vue'
 
 export default defineComponent({
   name: 'ActionPatentOffice',
@@ -29,7 +33,8 @@ export default defineComponent({
   },
   components: {
     ActionBox,
-    AppIcon
+    AppIcon,
+    HydroCost
   },
   props: {
     actionItem: {
@@ -49,7 +54,29 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.action {
+  position: relative;
+}
 .icon {
-  width: 4rem;
+  position: absolute;
+  top: 16px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  height: 2.5rem;
+  z-index: 10;
+  &.conduit {
+    top: 12px;
+    height: 3rem;
+  }
+}
+.hydroCost {
+  position: absolute;
+  z-index: 20;
+  left: 0;
+  bottom: 0;
+}
+.tile {
+  width: 12rem;
+  filter: drop-shadow(0.1rem 0.1rem 0.4rem #e4ae54);
 }
 </style>
